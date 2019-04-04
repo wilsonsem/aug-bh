b1547086c80ca8ad4db40e81cb526954610d96ad
Semmwa Wilson Bakshak <wilsonsemmwa@gmail.com>
2019-04-04 14:19:39 +0100

    update on urls

--- augbh/blog/urls.py
+++ augbh/blog/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from blog import views
 
 #url for pages
 urlpatterns = [
-	url(r'^events$', views.events, name='events.html'),
-	url(r'^$', views.index, name='index.html'),
-	url(r'^post1$', views.post1, name='post1.html'),
-	url(r'^post2$', views.post2, name='post2.html'),
-	url(r'^post3$', views.post3, name='Post3.html'),
+	path('events/', views.events, name="events"),
+	path('', views.index, name="index"),
+	path('post1/', views.post1, name="post1"),
+	path('post2/', views.post2, name="post2"),
+	path('post3/', views.post3, name="Post3"),
 ]
