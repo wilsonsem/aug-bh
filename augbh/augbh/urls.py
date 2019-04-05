1808f8f8987cdf8c17ea366588a4b68f68ced2bc
felexkemboi <felokemboi10@gmail.com>
2019-04-05 16:00:27 +0300

    add gitignore file and rewrite the urls file in blog app in the correct way

--- augbh/augbh/urls.py
+++ augbh/augbh/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include , url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
--- augbh/blog/urls.py
+++ augbh/blog/urls.py
@@ -1,12 +1,28 @@
+"""
 from django.conf.urls import url
 from . import views
+
 from blog import views
 
 #url for pages
 urlpatterns = [
-	url(r'^events$', views.events, name='events.html'),
+	path(r'^events$', views.events, name='events.html'),
 	url(r'^$', views.index, name='index.html'),
 	url(r'^post1$', views.post1, name='post1.html'),
 	url(r'^post2$', views.post2, name='post2.html'),
 	url(r'^post3$', views.post3, name='Post3.html'),
+]
+"""
+
+#compare how you have written your code with how it could be done better
+from django.urls import path
+
+#from the current directory import view
+from . import views
+
+urlpatterns = [
+	path('events/', views.events, name='events'),
+	path('post1/',  views.post1,  name='post1'),
+    path('post2/',  views.post2,  name='post2'),
+	path('post3/',  views.post1,  name='post3'),
 ]
