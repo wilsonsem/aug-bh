7a06231f755baeacca457c2e3c3247c11bbbc49f
Semmwa Wilson Bakshak <wilsonsemmwa@gmail.com>
2019-05-12 14:25:33 +0100

    template error

--- augbh/augbh/settings.py
+++ augbh/augbh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'augbh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 EMAIL_BAKEND ='django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST='localhost'
 EMAIL_PORT=1025
